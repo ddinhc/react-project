8685aaddf3ab62c0bdf1042d968166111894b18f
ddinhc <duy.dinhcs@gmail.com>
2022-03-12 17:05:58 -0800

    Updates

---
+++ precache-manifest.73d3039c62026e34d5e287cd04ff7fbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52d7da6d1692bcec3fa2b49ee1f23e7",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "0a59a20846a48ffcffc1",
+    "url": "/react-project/static/css/main.e33aa362.chunk.css"
+  },
+  {
+    "revision": "16726a010a335ff776de",
+    "url": "/react-project/static/js/2.138cb658.chunk.js"
+  },
+  {
+    "revision": "0a59a20846a48ffcffc1",
+    "url": "/react-project/static/js/main.19eacf5d.chunk.js"
+  },
+  {
+    "revision": "c38e84bee75be2db3df6",
+    "url": "/react-project/static/js/runtime~main.270f79fb.js"
+  },
+  {
+    "revision": "8b85b0c134b893b19eff2694c649670e",
+    "url": "/react-project/static/media/CardImage.8b85b0c1.jpg"
+  },
+  {
+    "revision": "a193b7b49373a9bbaeab963cdc6b9433",
+    "url": "/react-project/static/media/jumbotron.a193b7b4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73ea413ca1ac4ee95d6a9bda28444c00.js"
+  "/react-project/precache-manifest.73d3039c62026e34d5e287cd04ff7fbe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
